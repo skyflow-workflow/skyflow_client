d396e944e0ba918b71d2ed525d621cd95b3e1a33
mmtbak <47457296+mmtbak@users.noreply.github.com>
2025-12-01 02:46:19 +0800

    add stage

--- client_test.go
+++ client_test.go
@@ -5,49 +5,6 @@ import (
 	"testing"
 )
 
-var sf_obj *SkyFlowClient
-
-func init() {
-	var err error
-	server := "http://9.134.6.17:8080/"
-	// server := "9.134.6.17:8080"
-	sf_obj, err = NewSkyFlowClient(server)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	fmt.Println(sf_obj)
-}
-
-func TestSkyFlow(t *testing.T) {
-
-	data, err := sf_obj.ListRepositories()
-	if err != nil {
-		fmt.Println("error")
-		fmt.Println(err.Error())
-		return
-	}
-	fmt.Println(data)
-	for _, re := range data {
-		fmt.Println(re.Name, re.Comment)
-		acts, err := sf_obj.ListActivities(re.Name)
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-		for _, act := range acts {
-			fmt.Println(act.Name)
-			actinfo, err := sf_obj.DescribeActivity(act.URI)
-			if err != nil {
-				fmt.Println(err.Error())
-				return
-			}
-			fmt.Println(actinfo)
-		}
-	}
-
-}
-
 func TestDescribeActivity(t *testing.T) {
 
 	var repoName = "qyweixin"
